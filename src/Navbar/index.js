7ee0e4a8250a031193043dfe6efcc8c92b538c22
Mirza Baig <mirzabaig@Mirzas-MacBook-Pro.local>
2019-01-17 23:50:11 -0600

    added navbar and overall color convention

---
+++ src/Navbar/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -18,
+    marginRight: 10,
+  },
+};
+
+function DenseAppBar(props) {
+  const { classes } = props;
+  return (
+    <div className={classes.root}>
+      <AppBar position="absolute" style={{ backgroundColor: '#2E6A8A', width: '98%', top: '15px', left: 15 }}>
+        <Toolbar variant="dense">
+        <IconButton className={classes.menuButton} color='inherent' aria-label="Menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" style={{color:'#ECEEF9'}}>
+            Pints & Shells
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+DenseAppBar.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(DenseAppBar);
